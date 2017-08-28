3b3bb9b9c680825da4951adf1bd63092449b0190
BrandonRomano <BrandonRRomano@gmail.com>
2017-08-28 16:19:55 -0400

    Update tests to handle null cases

--- null_handlers_test.go
+++ null_handlers_test.go
@@ -20,6 +20,13 @@ func TestNullInt(t *testing.T) {
 	assert.Equal(t, int64(12), id.Int64)
 	assert.True(t, id.Valid)
 
+	// Test null case
+	var nullId null.Int
+	err = v.Validate([]*v.Value{
+		{Result: &nullId, Name: "id", Input: "", TypeHandler: v.NullIntHandler},
+	})
+	assert.False(t, nullId.Valid)
+
 	// Test failure case
 	var failureId null.Int
 	err = v.Validate([]*v.Value{
@@ -39,6 +46,13 @@ func TestNullString(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "hello", slug.String)
 	assert.True(t, slug.Valid)
+
+	// Test null case
+	var nullSlug null.String
+	err = v.Validate([]*v.Value{
+		{Result: &nullSlug, Name: "slug", Input: "", TypeHandler: v.NullStringHandler},
+	})
+	assert.False(t, nullSlug.Valid)
 }
 
 // TestNullFloat tests handling of a null.Float as the result
@@ -51,6 +65,13 @@ func TestNullFloat(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, float64(12.8), id.Float64)
 	assert.True(t, id.Valid)
+
+	// Test null case
+	var nullId null.Float
+	err = v.Validate([]*v.Value{
+		{Result: &nullId, Name: "id", Input: "", TypeHandler: v.NullFloatHandler},
+	})
+	assert.False(t, nullId.Valid)
 
 	// Test failure case
 	var failureId null.Float
@@ -72,6 +93,13 @@ func TestNullBool(t *testing.T) {
 	assert.True(t, someBool.Bool)
 	assert.True(t, someBool.Valid)
 
+	// Test null case
+	var nullBool null.Bool
+	err = v.Validate([]*v.Value{
+		{Result: &nullBool, Name: "some_bool", Input: "", TypeHandler: v.NullBoolHandler},
+	})
+	assert.False(t, nullBool.Valid)
+
 	// Test failure case
 	var someOtherBool null.Bool
 	err = v.Validate([]*v.Value{
@@ -92,6 +120,13 @@ func TestNullTime(t *testing.T) {
 	assert.Equal(t, successTime.Time.Year(), 2012)
 	assert.Equal(t, successTime.Time.Month(), time.November)
 	assert.Equal(t, successTime.Time.Day(), 1)
+
+	// Test null case
+	var nullTime null.Time
+	err = v.Validate([]*v.Value{
+		{Result: &nullTime, Name: "time", Input: "", TypeHandler: v.NullTimeHandler},
+	})
+	assert.False(t, nullTime.Valid)
 
 	// Test failure case
 	var errorTime null.Time
